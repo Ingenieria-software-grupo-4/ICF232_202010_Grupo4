0d9aa7c7454d2fc1cd5f1216d4a82c8348548b90
AlexanderMercado <alexpmercado@gmail.com>
2020-12-05 00:35:27 -0300

    cambio base de datos a postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_03_210512) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "nombre"
     t.string "tipo"
